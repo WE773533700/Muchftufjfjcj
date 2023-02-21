7c1a54f605cade73fb055ce12a0296b6ca5de303
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-02-21 17:58:02 +0300

    Update play.py

--- ShizukaXMusic/plugins/play/play.py
+++ ShizukaXMusic/plugins/play/play.py
@@ -27,24 +27,6 @@ from ShizukaXMusic.utils.inline.playlist import botplaylist_markup
 from ShizukaXMusic.utils.logger import play_logs
 from ShizukaXMusic.utils.stream.stream import stream
 
-
-
-force_btn = InlineKeyboardMarkup(
-    [
-        [                                                      
-        ],        
-    ]
-)
-
-async def check_is_joined(message):    
-    try:
-        userid = message.from_user.id
-        status = await app.get_chat_member("S_U_J", userid)
-        return True
-    except Exception:
-        await message.reply_text("*انت لست مشترك في قناة البوت @S_U_J ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
-        return False
-        
         
         
        
