34d8528bccde52a753e46e58b688697fe3ed4d46
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-02-21 15:55:56 +0300

    Update skip.py

--- ShizukaXMusic/plugins/admins/skip.py
+++ ShizukaXMusic/plugins/admins/skip.py
@@ -18,14 +18,10 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
 @app.on_message(
-    filters.command(SKIP_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
-)
-@app.on_message(
     filters.command(["التالي","لندا غيري","تخطي"],"") & filters.group & ~filters.edited & ~BANNED_USERS
-
+)
 @AdminRightsCheck    
-async def skip(cli, message: Message, _, chat_id):
-   
+async def skip(cli, message: Message, _, chat_id): 
     if not len(message.command) < 2:
         loop = await get_loop(chat_id)
         if loop != 0:
