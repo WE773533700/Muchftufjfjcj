dfe21fa5a846a8324809440c038e7fe329659c5a
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-02-23 21:10:51 +0300

    Update play.py

--- ShizukaXMusic/plugins/play/play.py
+++ ShizukaXMusic/plugins/play/play.py
@@ -122,10 +122,9 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
     
-@app.on_message(filters.command(["شغل","تشغيل","شغلي"],"")
-& ~filters.edited
-& ~BANNED_USERS)
-
+@app.on_message(
+    filters.command(PLAY_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+)
 @PlayWrapper
 
 async def play_commnd(
